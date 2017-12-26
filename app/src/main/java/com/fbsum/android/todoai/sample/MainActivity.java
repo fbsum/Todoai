b2da08000f0eb5e6c4165bd708e03bd243f021ef
sum <scausum@gmail.com>
2017-12-26 16:21:17 +0800

    update sample

--- app/src/main/java/com/fbsum/android/todoai/sample/MainActivity.java
+++ app/src/main/java/com/fbsum/android/todoai/sample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fbsum.android.todoai.sample;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +22,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void test() {
+        Context context = this;
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + 864000000L;
+        boolean result = false;
+        String tag = "SHOW_TAG";
+        result = Todoai.persist().isDone(tag);
+        result = Todoai.persist().today().isDone(tag);
+        result = Todoai.persist().newVersion(context).isDone(tag);
+        result = Todoai.persist().afterInstall(context).isDone(tag);
+        result = Todoai.persist().between(startTime, endTime).isDone(tag);
+        // ...
+
+        result = Todoai.persist().moreThan(5).check(tag);
+        result = Todoai.persist().exactly(5).check(tag);
+        result = Todoai.persist().lessThan(5).check(tag);
+        result = Todoai.persist().today().moreThan(5).check(tag);
+        result = Todoai.persist().newVersion(context).exactly(5).check(tag);
+        result = Todoai.persist().between(startTime, endTime).lessThan(5).check(tag);
+        // ...
+
+        result = Todoai.session().isDone(tag);
+        result = Todoai.session().today().isDone(tag);
+        result = Todoai.session().between(startTime, endTime).isDone(tag);
+        // ...
+
+        result = Todoai.session().moreThan(5).check(tag);
+        result = Todoai.session().exactly(5).check(tag);
+        result = Todoai.session().lessThan(5).check(tag);
+        result = Todoai.session().between(startTime, endTime).lessThan(5).check(tag);
+        // ...
+
+        Todoai.persist().makeDone(tag);
+        Todoai.session().makeDone(tag);
+
         if (Todoai.persist().newVersion(this).check("SHOW_GUIDE")) {
             Log.e(TAG, "show guide ...");
             Todoai.persist().makeDone("SHOW_GUIDE");
